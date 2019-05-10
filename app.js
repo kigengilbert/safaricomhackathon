2e46c1a9d89f930c4101d818388c1ee7518b736f
Kigen Gilbert <gilbertkiproz@gmail.com>
2019-05-10 11:52:57 +0300

    prov test 2

--- app.js
+++ app.js
@@ -6,15 +6,28 @@ function run() {
     const app = express()
     var cors = require('cors');
     var bodyParser = require('body-parser')
+    const mysql = require('mysql');
     app.use(cors());
     app.use(bodyParser.urlencoded({ extended: true }))
 // parse application/json
     app.use(bodyParser.json())
-
-    // body parser middleware
-  
-  
     var server_port = process.env.YOUR_PORT || process.env.PORT || 80
+    // mysql
+    const db = mysql.createConnection ({
+        host: 'www.db4free.net',
+        user: 'gkigen',
+        password: '2YCGQhR4V9dfZ4L',
+        database: 'safaricomhack'
+    });
+    
+    db.connect((err) => {
+        if (err) {
+            throw err;
+        }
+        console.log('Connected to database');
+    });
+    global.db = db;
+   
      // setup directory for static files (html, css, etc.)
      app.use(express.static('public'));
 
@@ -24,8 +37,13 @@ function run() {
      });
      
 app.post('/provision',function(req,res){
- 
+    
+    let sql = "INSERT INTO 'simdb' ('ICCID', 'IMSI', 'Ki', 'PIN1', 'PUC', 'status') VALUES ('1234567', '1234567', 'ssds', '1245', '144556', '1')"
     console.log("IMSI: " +req.body.IMSI)
+    db.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
     res.json({ message: "yes...." + req.body.ICCID + "  has been sent"  });
 })
       
