1d1e8d423fe39943769aaaaa0a7dbfdac90dd5b0
Kigen Gilbert <gilbertkiproz@gmail.com>
2019-05-10 10:32:10 +0300

    prov

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ function run() {
     // express server
     const express = require('express')
     const app = express()
+    const bodyParser = require("body-parser");
+
+    // body parser middleware
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+  
     var server_port = process.env.YOUR_PORT || process.env.PORT || 80
      // setup directory for static files (html, css, etc.)
      app.use(express.static('public'));
+     const prov = require("./api/provision");
+
      
      app.get('/', function (req, res) {
          res.sendFile('/public/index.html');
@@ -14,9 +22,8 @@ function run() {
      
      app.post('/provision', function (req, res) {
         res.sendFile('/public/provision.html');
-     app.get('/a', function (req, res) {
-            res.sendFile('/public/index.html');
-        });
+
+    app.use("/api/provision", prov);
     });   
     app.listen(server_port, () => console.log('Example app listening !'))
 }
