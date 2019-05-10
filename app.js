005ee07b6b513d3df31da44d4b9df0dfdea62110
Kigen Gilbert <gilbertkiproz@gmail.com>
2019-05-10 12:35:39 +0300

    prov 11

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function run() {
      
 app.post('/provision',function(req,res){
     
-   let sql = "INSERT INTO 'simdb' ('ICCID', 'IMSI', 'Ki', 'PIN1', 'PUC', 'status') VALUES ('1234567', '1234567', 'ssds', '1245', '144556', '1')"
+   let sql = "INSERT INTO simdb ('ICCID', 'IMSI', 'Ki', 'PIN1', 'PUC', 'status') VALUES ('1234567', '1234567', 'ssds', '1245', '144556', '1')"
     //var sql = "INSERT INTO simdb (ICCID, IMSI,Ki,PIN1,PUC,status) VALUES (" + req.body.ICCID +","+ req.body.IMSI+","+req.body.Ki+","+req.body.PIN1+","+req.body.PUC+","+req.body.Status+")"
     console.log("IMSI: " +req.body.IMSI)
     console.log("ICCID: " +req.body.ICCID)
