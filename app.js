e12eaecf7e168016a522d9f1da9515c1354f794b
Kigen Gilbert <gilbertkiproz@gmail.com>
2019-05-10 12:41:16 +0300

    prov 13

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.post('/provision',function(req,res){
             if (err) throw err;
             console.log("1 record inserted");
           });
-          db.close()
+          db.end()
     });
     
     res.json({ message: "yes...." + req.body.ICCID + "  has been sent"  });
