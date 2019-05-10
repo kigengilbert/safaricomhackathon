0006cb09686fd1673d985c5cc7fc80ac61227350
Kigen Gilbert <gilbertkiproz@gmail.com>
2019-05-10 10:02:41 +0300

    provision api

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ function run() {
     var server_port = process.env.YOUR_PORT || process.env.PORT || 80
      // setup directory for static files (html, css, etc.)
      app.use(express.static('public'));
-
+     const provisionSIM = require("./api/provision");
      // server root: static index.html
+     app.use("/api/provision", provisionSIM);
      app.get('/', function (req, res) {
-         res.sendFile('index.html');
+         res.sendFile('/public/index.html');
      });
         
     app.listen(server_port, () => console.log('Example app listening !'))
