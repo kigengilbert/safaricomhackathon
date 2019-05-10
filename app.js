29df9b7eb1a9c900ccd17fb078c67068a51d8fc6
Kigen Gilbert <gilbertkiproz@gmail.com>
2019-05-10 11:27:00 +0300

    prov test 1

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ function run() {
      
 app.post('/provision',function(req,res){
  
+    console.log("IMSI: " +req.body.IMSI)
     res.json({ message: "yes...." + req.body.message + "  has been sent"  });
 })
       
