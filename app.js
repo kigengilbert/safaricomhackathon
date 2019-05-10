9d67bc0f30646021f82d6bb9d9c8c8dc56772cfe
Kigen Gilbert <gilbertkiproz@gmail.com>
2019-05-10 10:09:40 +0300

    provision api change

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ function run() {
     var server_port = process.env.YOUR_PORT || process.env.PORT || 80
      // setup directory for static files (html, css, etc.)
      app.use(express.static('public'));
-     const provisionSIM = require("./api/provision");
-     // server root: static index.html
-     app.use("/api/provision", provisionSIM);
+     
      app.get('/', function (req, res) {
          res.sendFile('/public/index.html');
      });
-        
+     
+     app.get('/api/provision', function (req, res) {
+        res.sendFile('/public/provision.html');
+    });   
     app.listen(server_port, () => console.log('Example app listening !'))
 }
